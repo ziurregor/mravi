ad546344fdad3ed7ed8f31cd6306f9154f041653
Roger.Ruiz <rruiz_1@hotmail.com>
2019-04-03 14:14:25 -0400

    Add project files.

---
+++ mravi/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace mravi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                string[] split = line.Split(new char[] { ' ' }, StringSplitOptions.None);
+                long a = Int64.Parse(split[0]);
+                long b = Int64.Parse(split[1]);
+                Console.WriteLine(Math.Abs(a - b));
+            }
+
+
+
+
+
+                //int l;
+                //Console.WriteLine("Nvari");
+
+                //l= Convert.ToInt32(Console.ReadLine());
+
+                //Console.WriteLine(l);
+
+            }
+    }
+}
